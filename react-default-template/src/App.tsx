9c609f5605bab51770b694972c29006a36181aba
PhilReact <philliplangmartinez@gmail.com>
2025-03-10 16:58:26 +0200

    adde qapp-core

--- react-default-template/src/App.tsx
+++ react-default-template/src/App.tsx
@@ -1,10 +1,13 @@
 import { Typography } from "@mui/material"
+import { useGlobal } from "qapp-core"
 
+ 
 function App() {
+  const {auth} = useGlobal()
 
   return (
     <>
-     <Typography>Welcome to Qortal</Typography>
+     <Typography>Welcome to Qortal {auth?.name}</Typography>
     </>
   )
 }
--- react-default-template/src/main.tsx
+++ react-default-template/src/main.tsx
@@ -1,13 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Routes } from './Routes.tsx'
-import ThemeProviderWrapper from './styles/theme/theme-provider.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Routes } from "./Routes.tsx";
+import ThemeProviderWrapper from "./styles/theme/theme-provider.tsx";
+import { GlobalProvider } from "qapp-core";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProviderWrapper>
-    <Routes />
-    </ThemeProviderWrapper>
-  </StrictMode>,
-)
+    <GlobalProvider
+      config={{
+        auth: {
+          balanceSetting: {
+            interval: 180000,
+            onlyOnMount: false,
+          },
+          authenticateOnMount: true,
+        },
+      }}
+    >
+      <ThemeProviderWrapper>
+        <Routes />
+      </ThemeProviderWrapper>
+    </GlobalProvider>
+  </StrictMode>
+);
