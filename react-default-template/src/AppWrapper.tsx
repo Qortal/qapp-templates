3ff1d327a95650333270e68f69636745d26f604c
PhilReact <philliplangmartinez@gmail.com>
2025-04-04 03:33:16 +0300

    fixes

--- react-default-template/src/AppWrapper.tsx
+++ react-default-template/src/AppWrapper.tsx
@@ -1,11 +1,8 @@
 import { Routes } from "./Routes";
 import { GlobalProvider } from "qapp-core";
 import { publicSalt } from "./qapp-config.ts";
-import { EnumTheme, useSystemState } from "./state/global/system";
-import { darkTheme, lightTheme } from "./styles/theme/theme";
 
 export const AppWrapper = () => {
-  const theme = useSystemState().theme;
   return (
     <GlobalProvider
       config={{
@@ -17,9 +14,7 @@ export const AppWrapper = () => {
           authenticateOnMount: true,
         },
         publicSalt: publicSalt,
-        appName: "q-search",
       }}
-      parentTheme={theme === EnumTheme.LIGHT ? lightTheme : darkTheme}
     >
       <Routes />
     </GlobalProvider>
