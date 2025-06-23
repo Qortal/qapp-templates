69800ba676a22511696c7945b08859f59d796980
PhilReact <philliplangmartinez@gmail.com>
2025-06-23 21:13:08 +0300

    fix layout appwrapper

--- react-default-template/src/AppWrapper.tsx
+++ react-default-template/src/AppWrapper.tsx
@@ -1,28 +1,27 @@
-import { Routes } from './routes/Routes.tsx';
-import { GlobalProvider } from 'qapp-core';
-import { publicSalt } from './qapp-config.ts';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { GlobalProvider } from 'qapp-core'
+import Layout from './styles/Layout'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { publicSalt } from './qapp-config';
 
 export const AppWrapper = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
+    const navigate = useNavigate()
+    const location = useLocation()
   return (
     <GlobalProvider
-      config={{
-        auth: {
-          balanceSetting: {
-            interval: 180000,
-            onlyOnMount: false,
-          },
-          authenticateOnMount: true,
-        },
-        publicSalt: publicSalt,
-      }}
-      navigate={navigate}
-      location={location}
-    >
-      <Routes />
-    </GlobalProvider>
-  );
-};
+            config={{
+              auth: {
+                balanceSetting: {
+                  interval: 180000,
+                  onlyOnMount: false,
+                },
+                authenticateOnMount: true,
+              },
+              publicSalt: publicSalt,
+            }}
+            navigate={navigate}
+            location={location}
+          >
+            <Layout />
+          </GlobalProvider>
+  )
+}
