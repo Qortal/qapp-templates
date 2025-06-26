58405074fe3585f7d870ba246c8d2824239de970
PhilReact <philliplangmartinez@gmail.com>
2025-06-26 22:59:06 +0300

    change import

--- react-default-template/src/main.tsx
+++ react-default-template/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ThemeProviderWrapper from './styles/theme/theme-provider.tsx';
-import {  Routes } from './Routes.tsx';
 import './index.css';
 import './i18n/i18n.ts'
+import { Routes } from './routes/Routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
